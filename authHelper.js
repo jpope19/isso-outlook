02026537e80587e41f5cfad256a9ef1b6d669097
Pope <jeffrey.pope@isso.net>
2017-03-13 10:39:11 -0400

    hello world

--- authHelper.js
+++ authHelper.js
@@ -1,7 +1,7 @@
 var credentials = {
   client: {
     id: 'YOUR APP ID HERE',
-    secret: 'YOUR APP PASSWORD HERE',
+    secret: 'YOUR APP PASSWORD HERE'
   },
   auth: {
     tokenHost: 'https://login.microsoftonline.com',
@@ -9,12 +9,13 @@ var credentials = {
     tokenPath: 'common/oauth2/v2.0/token'
   }
 };
-var oauth2 = require('simple-oauth2')(credentials);
+var oauth2 = require('simple-oauth2').create(credentials);
 
 var redirectUri = 'http://localhost:8000/authorize';
 
 // The scopes the app requires
 var scopes = [ 'openid',
+               'offline_access',
                'https://outlook.office.com/mail.read' ];
     
 function getAuthUrl() {
@@ -26,4 +27,29 @@ function getAuthUrl() {
   return returnVal;
 }
 
+function getTokenFromCode(auth_code, callback, response) {
+  var token;
+  oauth2.authorizationCode.getToken({
+    code: auth_code,
+    redirect_uri: redirectUri,
+    scope: scopes.join(' ')
+  }, function (error, result) {
+    if (error) {
+      console.log('Access token error: ', error.message);
+      callback(response, error, null);
+    } else {
+      token = oauth2.accessToken.create(result);
+      console.log('Token created: ', token.token);
+      callback(response, null, token);
+    }
+  });
+}
+
+function refreshAccessToken(refreshToken, callback) {
+  var tokenObj = oauth2.accessToken.create({refresh_token: refreshToken});
+  tokenObj.refresh(callback);
+}
+
 exports.getAuthUrl = getAuthUrl;
+exports.getTokenFromCode = getTokenFromCode;
+exports.refreshAccessToken = refreshAccessToken;
